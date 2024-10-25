4b53e84be9fd9eae6e187ad19912ab86cbf90f12
Pranav M <pranavm265@gmail.com>
2024-10-25 14:01:35 +0530

    quick sort

---
+++ Sorting/QuickSort.java
@@ -0,0 +1,51 @@
+package Sorting;
+
+public class QuickSort {
+
+    public static void quickSort(int[] array, int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(array, low, high);
+            quickSort(array, low, pivotIndex - 1);
+            quickSort(array, pivotIndex + 1, high);
+        }
+    }
+
+    private static int partition(int[] array, int low, int high) {
+        int pivot = array[high];
+        int i = (low - 1);
+
+        for (int j = low; j < high; j++) {
+            if (array[j] <= pivot) {
+                i++;
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+
+        int temp = array[i + 1];
+        array[i + 1] = array[high];
+        array[high] = temp;
+
+        return i + 1;
+    }
+
+    public static void main(String[] args) {
+        int[] array = { 10, 7, 8, 9, 1, 5 };
+        System.out.println("Original array:");
+        printArray(array);
+
+        quickSort(array, 0, array.length - 1);
+
+        System.out.println("Sorted array:");
+        printArray(array);
+    }
+
+    // Method to print the array
+    private static void printArray(int[] array) {
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
