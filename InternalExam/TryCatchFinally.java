b4133f4de1fc88c3f51410ff04596703dce2b52a
Pranav M <pranavm265@gmail.com>
2024-11-08 12:12:01 +0530

    Update TryCatchFinally.java

--- InternalExam/TryCatchFinally.java
+++ InternalExam/TryCatchFinally.java
@@ -1,14 +1,17 @@
 package InternalExam;
 
+import java.util.Scanner;
+
 class TryCatchFinally {
     public static void main(String args[]) throws ArithmeticException {
-        int a = args.length;
-        int d = 45;
+        Scanner src = new Scanner(System.in);
+        System.out.println("Enter two numbers: ");
+        int a = src.nextInt();
+        int b = src.nextInt();
         try{
-            if(a == 0){
-                d = d/a-1;
-                throw new ArithmeticException("ZeroZeroZZEERRROO");
-            }
+            if(a/b == 0)
+                    throw new ArithmeticException("Division by Zero Encountered");
+            System.out.println("Doing a/b: " a/b);
         }
         catch(ArithmeticException ae){
             System.out.println("Exception Caught Successfully: " + ae);
