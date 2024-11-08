f3e83db6a5c5a1a5ba451ac6eeb078d86a7d5ce3
Pranav M <pranavm265@gmail.com>
2024-11-08 12:39:57 +0530

    Update MultiThreading.java

--- Threads/MultiThreading.java
+++ Threads/MultiThreading.java
@@ -4,21 +4,19 @@ import java.util.Random;
 
 class ThreadOne extends Thread {
     Random rnd = new Random();
-    ThreadTwo t2;
-    ThreadThree t3;
-
-    ThreadOne(ThreadTwo t2, ThreadThree t3) {
-        this.t2 = t2;
-        this.t3 = t3;
-    }
 
     public void run() {
         try {
             for (int i = 0; i < 10; i++) {
                 int randomInt = rnd.nextInt(1000);
                 System.out.println("Generated Random Int: " + randomInt);
-                t2.processNumber(randomInt);
-                t3.processNumber(randomInt);
+                if(randomInt % 2 == 0){
+                    ThreadTwo t2 = new ThreadTwo(randomInt);
+                    t2.start();
+                } else {
+                    ThreadThree t3 = new ThreadThree(randomInt);
+                    t3.start();
+                }
                 Thread.sleep(1000);
             }
         } catch (InterruptedException e) {
@@ -28,31 +26,30 @@ class ThreadOne extends Thread {
 }
 
 class ThreadTwo extends Thread {
-    public void processNumber(int number) {
+    int number;
+    ThreadTwo(int number){
+        this.number = number;
+    }
+    public void run() {
         if (number % 2 == 0)
             System.out.println("Even: " + number + " | Square: " + (number * number));
-
-    }
-
-    public void run() {
     }
 }
 
 class ThreadThree extends Thread {
-    public void processNumber(int number) {
-        if(number % 2 != 0)
-            System.out.println("Odd: " + number + " | Cube: " + (number * number * number));            
+    int number;
+    ThreadThree(int number){
+        this.number = number;
     }
-
     public void run() {
+        if(number % 2 != 0)
+            System.out.println("Odd: " + number + " | Cube: " + (number * number * number));    
     }
 }
 
 class MultiThreading {
     public static void main(String args[]) {
-        ThreadThree t3 = new ThreadThree();
-        ThreadTwo t2 = new ThreadTwo();
-        ThreadOne t1 = new ThreadOne(t2,t3);
+        ThreadOne t1 = new ThreadOne();
         t1.start();
     }
 }
