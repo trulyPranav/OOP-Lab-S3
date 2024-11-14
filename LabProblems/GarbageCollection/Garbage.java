8fde65b96f36f73f047eb5cd6379d4f65ca165ea
Pranav M <pranavm265@gmail.com>
2024-11-14 16:19:31 +0530

    GarbageCollection Done

---
+++ LabProblems/GarbageCollection/Garbage.java
@@ -0,0 +1,14 @@
+package LabProblems.GarbageCollection;
+
+// Write a Java program to demonstrate the use of garbage collector.
+
+class Garbage {
+    protected void finalize(){
+        System.out.println("Garbage Collection Performed Successfully!");
+    }
+    public static void main(String[] args) {
+        Garbage g1 = new Garbage(), g2 = new Garbage();
+        g2 = null;
+        System.gc();
+    }
+}
